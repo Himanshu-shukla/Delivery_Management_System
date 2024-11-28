b93bebaeb540943c8816e01651d1538644862064
Airline Plan <hhimanshu030@gmail.com>
2024-11-28 08:39:09 +0530

    db update

--- src/db.ts
+++ src/db.ts
@@ -2,11 +2,11 @@ import mongoose from 'mongoose';
 
 export async function connectDB() {
   try {
-    const mongoURI = 'mongodb://localhost:27017/delivery_management_db'; // Replace with your MongoDB URI
+    const mongoURI = 'mongodb+srv://crazyphoton150hs:C00!buddy@cluster0.4mq6pjf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'; 
     await mongoose.connect(mongoURI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    process.exit(1); // Exit process with failure
+    process.exit(1); 
   }
 }
